8874549e047b3cbeefee407be4c9fafa9ff84267
rogeriopcce <rogeriopcce@gmail.com>
2020-07-04 13:40:30 -0300

    Curso Banco de Dados DAO

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,13 +1,6 @@
 package aplication;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import javax.swing.JOptionPane;
-
-import camada.conexao.DB;
-import camada.conexao.DbEntegrityException;
+import camada.model.Departamento;
 
 
 
@@ -15,31 +8,8 @@ public class Program {
 
 	public static void main(String[] args){
 
-		Connection conn = null;
-		PreparedStatement st = null;
-		
-
-		try {
-			conn = DB.getConnection();
-			st = conn.prepareStatement("DELETE FROM DEPARTMENT WHERE ID = ?");
-			
-			st.setInt(1, 2);
-
-			int totallinhas = st.executeUpdate();
-			
-			System.out.println("Total de linhas afetadas " + totallinhas);
-		} catch (SQLException e) {
-
-			//throw new DbEntegrityException(e.getMessage());
-			JOptionPane.showMessageDialog(null, e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
-			
-		} finally {
-			
-			DB.closeStatement(st);
-			DB.closeConnetion();
-			
-		}
-		
+		Departamento obj = new Departamento(1,"Financeiro"); 
+		System.out.println(obj);
 	}
 
 }
