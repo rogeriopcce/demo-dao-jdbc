86f560bd4d8b949072953976d64801af1fcfd7b5
rogeriopcce <rogeriopcce@gmail.com>
2020-07-05 12:39:30 -0300

    demo-dao-jdbc

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,6 +1,9 @@
 package aplication;
 
+import java.util.Date;
+
 import camada.model.Departamento;
+import camada.model.Funcionario;
 
 
 
@@ -10,6 +13,9 @@ public class Program {
 
 		Departamento obj = new Departamento(1,"Financeiro"); 
 		System.out.println(obj);
+		
+		Funcionario seller = new Funcionario(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj );
+		System.out.println(seller);
 	}
 
 }
